86dde35029270c28ff4f148f2cce2c20e64d4582
Dipto Neogi <81651700+Dipto07@users.noreply.github.com>
2024-04-30 23:40:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelector('input[type="button"][value="Select and Remove"]');
+    const dropdown = document.getElementById('colorSelect');
+
+    button.addEventListener('click', function() {
+
+		const selectedOption = dropdown.options[dropdown.selectedIndex];
+
+        dropdown.removeChild(selectedOption);
+    });
+});
